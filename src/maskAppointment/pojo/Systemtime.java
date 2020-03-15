c7dd021d251294d4e3a8752a5df439fcf7828358
Hongkaibin <1341541192@qq.com>
2020-03-15 14:57:47 +0800

    系统时间

---
+++ src/maskAppointment/pojo/Systemtime.java
@@ -0,0 +1,32 @@
+package maskAppointment.pojo;
+
+public class Systemtime {
+	private int  id;
+	private String opentime;
+	private String closetime;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getOpentime() {
+		return opentime;
+	}
+	
+	public String geClosetime() {
+		return closetime;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setOpentime(String opentime) {
+		this.opentime = opentime;
+	}
+	
+	public void setClosetime(String closetime) {
+		this.closetime = closetime;
+	}
+	
+}
